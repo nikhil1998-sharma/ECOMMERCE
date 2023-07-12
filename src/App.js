30541ab855ea70ea031fc7e97ae8842e20da7530
Nikhil Sharma <nikhiljangid1998@gmail.com>
2023-07-13 00:28:19 +0530

    First commit adding client components and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Categories from "./pages/categories/Categories";
+import Navbar from "./components/navbar/Navbar";
+import ProductDetails from "./pages/productDetails/ProductDetails";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Appp">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Categories />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/categories/Categories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Categories.scss";
+
+function Categories() {
+  return <div className=""></div>;
+}
+
+export default Categories;
